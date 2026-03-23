2219ff05a20ca85c1b126441cee9fb7972b10a48
psychicworldconnection <psychicworldconnection@gmail.com>
2026-03-22 21:49:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ const fl = document.createElement("link");
 fl.href = "https://fonts.googleapis.com/css2?family=Russo+One&family=Nunito:wght@400;600;700;800;900&display=swap";
 
 
-fl.rel = “stylesheet”;
+fl.rel = "stylesheet";
+
 document.head.appendChild(fl);
 
 // ── THEME ──
